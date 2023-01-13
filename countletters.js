58c6f979a38c12543c2be5c3ff87b7b336617025
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-01-13 13:30:43 +0100

    Create countletters.js

---
+++ countletters.js
@@ -0,0 +1,18 @@
+// Name: Nathália Pimentel de Assis
+//University: University of Brasilia
+//Bachelor student in eletronic engineering course
+//Date: November 4th 2022
+
+function contaLetras(string, letra) {
+    let cl =0;
+    let i=0;
+ 
+    for(i=0; i< string.length; i++){
+        if(string[i] == letra){
+            cl = cl+1;
+        } else {
+            cl=cl;
+        }
+    }
+return cl
+}
